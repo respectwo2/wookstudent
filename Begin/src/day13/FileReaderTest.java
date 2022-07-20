5c21201ba00c5409dcb1e34e7ee59a5689e15a59
wook2 <respectwo@gmail.com>
2022-07-20 14:16:01 +0900

    day13.java Stream,FileReading

---
+++ Begin/src/day13/FileReaderTest.java
@@ -0,0 +1,39 @@
+package day13;
+/*	
+ * 	- 2byte(문자) 기반 스트림
+ * 	- 노드 스트림 (파일과 노드 연결)
+ * 
+ */
+import java.io.*;
+public class FileReaderTest {
+
+	public static void main(String[] args) 
+	throws IOException
+	{
+		String fname="C:\\Users\\성욱박\\git\\wookstudent\\Begin\\src\\day13\\FileInputStreamTest.java";
+		//String fname="C:/myjava/readme.txt";
+		
+		File file=new File(fname);
+		File file2=new File("C:/myjava/readme_copy2.txt");
+		
+		long fsize=file.length();// 파일의 크기를 반환
+		System.out.println("파일의 크기: "+fsize+"bytes");
+		
+		FileReader fr=new FileReader(file); //노드 연결
+		
+		FileWriter fw=new FileWriter(file2, true); //true를 주면 파일 내용을 덮어쓰지않고 덭붙여쓰기를한다.
+		
+		int n=0;
+		char[] data=new char[1000];
+		
+		while((n=fr.read(data))!=-1) {
+			fw.write(data, 0, n);
+			fw.flush();
+			//System.out.write(n);
+			//System.out.flush();
+		}
+		fr.close();
+
+	}
+
+}
